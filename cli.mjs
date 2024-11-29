268ad46186892a8dcbf65f159f38e39595212af1
Ryan Ramage <r@mage.rocks>
2024-11-29 11:05:26 -0700

    working with clack and picocolors

--- cli.mjs
+++ cli.mjs
@@ -2,11 +2,13 @@
 import rc from 'run-con'
 import { RoomManager, BreakoutRoom } from '@agree-able/room'
 import { handleInvite } from '@agree-able/invite'
-import { confirmRoomEnter, gatherExpectations, validateParticipant } from './userConfirmation.mjs'
+import { isCancel, intro, outro, log, text, spinner, note } from '@clack/prompts'
+import { validateAndUpdateConfig, confirmRoomEnter, gatherExpectations, validateParticipant } from './userConfirmation.mjs'
 
 const config = rc('breakout-room', {})
 
 async function run () {
+  await validateAndUpdateConfig(config)
   const { invite } = await handleInvite(config, confirmRoomEnter)
   if (invite) {
     const room = new BreakoutRoom({ invite })
@@ -16,23 +18,47 @@ async function run () {
   } else {
     const roomManager = new RoomManager()
     const expectations = await gatherExpectations(config)
-    const { agreeableKey } = await roomManager.startAgreeable(config, expectations, validateParticipant) 
-    console.log(`agreeableKey:`, agreeableKey)
-    roomManager.on('readyRoom', onRoom)
-    roomManager.installSIGHandlers() // handle shutdown signals
+    try {
+      const spin = spinner()
+      console.log('d')
+      spin.start('Starting breakout room manager...')
+      console.log('e')
+      const { agreeableKey } = await roomManager.startAgreeable(config, expectations, validateParticipant)
+      console.log('f')
+      spin.stop('breakout room running')
+      log.info(`agreeableKey: ${agreeableKey}`)
+      roomManager.on('readyRoom', onRoom)
+      roomManager.installSIGHandlers() // handle shutdown signals
+    } catch (err) {
+      console.log('error', err)
+      process.exit(1)
+    }
   }
 }
 
-function onRoom (room) {
-  roomPrint(room, `Room invite: ${room.getRoomInfo().invite}`);
-  process.stdin.on('data', async (data) => await room.message(data.toString()))
-  room.on('peerEntered', (peerKey) => console.log('peer entered the room', peerKey))
-  room.on('peerLeft', async (peerKey) => console.log('peer left the room', peerKey))
-  room.on('message', async (m) => console.log('remote message recieved', m))
+async function onRoom (room) {
+  log.info(`Room invite: ${room.getRoomInfo().invite}`)
+  let keepGoing = true
+  room.on('peerEntered', (peerKey) => log.info(`peer entered the room, ${peerKey}`))
+  room.on('peerLeft', async (peerKey) => log.info(`peer left the room, ${peerKey}`))
+  room.on('message', async (m) => log.info(`remote message recieved', ${JSON.stringify(m)}`))
+  while (keepGoing) {
+    const message = await text({
+      message: 'Send a message...',
+      placeholder: 'message to send',
+      validate (value) {
+        if (value.length === 0) return 'Value is required!'
+      }
+    })
+    if (isCancel(message)) {
+      keepGoing = false
+      await room.exit()
+      outro('Goodbye!')
+      process.exit(0)
+    }
+    room.message(message)
+  }
 }
 
-function roomPrint(room, message) {
-  console.log(`${room.getRoomInfo().roomId}: ${message}`);
-}
-
+intro('Welcome to breakout-room CLI!')
 run()
--- userConfirmation.mjs
+++ userConfirmation.mjs
@@ -1,23 +1,104 @@
+import { text, confirm, select, log, note } from '@clack/prompts'
+import pc from 'picocolors'
+
+const joinRoomValidate = async (config) => {
+  const { invite, agreeableKey, domain } = config
+  // need one of these
+  if (!invite && !agreeableKey && !domain) {
+    const howToConnect = await select({
+      message: 'how to bootstrap peer connection?',
+      options: [
+        { value: 'agreeableKey', label: 'use agreeableKey given to you', hint: 'reusable, but has terms to join' },
+        { value: 'invite', label: 'use a one time invite code' },
+        { value: 'domain', lable: 'use a domain txt lookup to find agreeable key' }
+      ]
+    })
+    const value = await text({
+      message: `Please enter the ${howToConnect}`,
+      initialValue: '',
+      validate (value) {
+        if (value.length === 0) return 'Value is required!'
+      }
+    })
+    config[howToConnect] = value
+  }
+}
+
+const startRoomManagerValidate = async (config) => {
+
+}
+
+const simpleRoomValidate = async (config) => {
+
+}
+
+export const validateAndUpdateConfig = async (config) => {
+  const { invite, agreeableKey, domain } = config
+  const joinRoom = invite || agreeableKey || domain
+
+  const { seed } = config
+  const startRoomManager = seed
+
+  if (joinRoom && !startRoomManager) return await joinRoomValidate(config)
+  if (startRoomManager && !joinRoom) return await startRoomManagerValidate(config)
+
+  // if none or both, lets clarify with the user
+  const projectType = await select({
+    message: 'What are you trying to do?',
+    options: [
+      { value: 'joinRoom', label: 'Join an existing room' },
+      { value: 'startRoomManager', label: 'Start a room manager', hint: 'host many rooms/connections' },
+      { value: 'simpleRoom', label: 'Start a simple room' }
+    ]
+  })
+  if (projectType === 'joinRoom') return await joinRoomValidate(config)
+  if (projectType === 'startRoomManager') return await startRoomManagerValidate(config)
+  return await simpleRoomValidate(config)
+}
+
 export const confirmRoomEnter = async (expectations, hostInfo) => {
-  console.log('We are about to enter a room that you have to agree to join')
-  console.log('here are the expectations')
-  console.log('', expectations, hostInfo)
-  // we should give the user a chance to accept these
-  // also if expectations.whoamiRequired, let them know they will be signing and sharing their info
-  return { rules: true, reason: true }
+  log.step('We are about to enter a room that you have to agree to join')
+  // log.info(`${pc.bold('room reason:')} ${pc.dim(expectations.reason)}`)
+  note(expectations.reason, `room reason`)
+  const reason = await confirm({
+    message: 'Do you agree to the room reason?'
+  })
+  // log.step(`${pc.bold('room rules:')} ${pc.dim(expectations.rules)}`)
+  note(expectations.rules, `room rules`)
+  const rules = await confirm({
+    message: 'Do you agree to the room rules?'
+  })
+  return { rules, reason }
 }
 
 export const gatherExpectations = async (config) => {
-  const expectations = {
-    reason: 'We are playing a game of 20 questions. The user is trying to guess an object. The assistant will provide hints and the user will ask yes/no questions to guess the object. The game ends when the user guesses the object or runs out of questions.',
-    rules: 'The user can ask yes/no questions to guess the object. The user has 20 questions to guess the object. The user can quit the game at any time by typing "quit". If the user guesses the object, the game ends. If the user runs out of questions, the game ends and the object is revealed.',
-    whoamiRequired: config.whoamiRequired || false
-  }
+  const reason = config.meaning || await text({
+    message: 'Give an explination of what will be accomplished in the room?',
+    placeholder: 'We are playing 20 questions',
+    initialValue: '',
+    validate (value) {
+      if (value.length === 0) return 'Value is required!'
+    }
+  })
+  const rules = config.rules || await text({
+    message: 'What are the room rules?',
+    placeholder: 'No profanity',
+    initialValue: '',
+    validate (value) {
+      if (value.length === 0) return 'Value is required!'
+    }
+  })
+  const whoamiRequired = config.whoamiRequired || await confirm({
+    message: 'Do you need participants to validate their identity with keybase?'
+  })
+  const expectations = { reason, rules, whoamiRequired }
+
   return expectations
 }
 
-export const validateParticipant = (acceptance, extraInfo) => {
-  console.log('asked to validate', acceptance, extraInfo)
-  return { ok: true }
+export const validateParticipant = async (acceptance, extraInfo) => {
+  const ok = await confirm({
+    message: 'Allow the participant to enter?'
+  })
+  return { ok }
 }
-
